084b3e8b137320b502500df4c7157f266fa9ce65
Christian Dalsvaag <christian@ddev.no>
2015-06-09 11:40:24 +0200

    Changed format of config

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
-var config = {}
+var secret = require('./.secret');
 
-config.socket = 42326;
+var config = {
+  socket: 42326,
+  db_host: "wave-chat.cotsyfcel8ra.eu-west-1.rds.amazonaws.com",
+  db_name: secret.db_name,
+  username: secret.username,
+  password: secret.password
+};
 
 module.exports = config;
