ce5ad2cb37bd6acf4ea4f35995a75d7655f73b7a
Daniel Vassdal <daniel@ddev.no>
2015-06-11 12:46:51 +0200

    command.js: Make commands modular

--- command.js
+++ command.js
@@ -1,13 +1,13 @@
 require('net');
-//var requireDir = require('require-dir');
+var requireDir = require('require-dir');
 
 var session = require('./session.js');
-//require('./commands');
+var cmdList = requireDir('./commands');
 
 var command = { };
 
 var testData = {
-    command: 'test',
+    command: 'authClient',
     params: {
       param1: 'one',
       param2: 'two'
@@ -15,7 +15,7 @@ var testData = {
   };
 
 command.handle = function(client, data) {
-  data = testData;
+//  data = testData;
 
   // If we're waiting for the result of auth,
   // save the command for later
@@ -24,37 +24,34 @@ command.handle = function(client, data) {
     return;
   }
 
-  try {
-    if (data.command === 'authClient')
-    {
-      if (session.isAuth(client))
-      {
-        console.warn('The client on %s tried to double auth. Disconnecting it.', client.remoteAddress);
-        client.end();
-        return;
-      }
+  if (data.command === undefined) {
+    console.warn('%s tried to run an empty command', client.remoteAddress);
+    client.end();
+    return;
+  }
 
-      // Create an array to store new messages from the client in as we wait
-      // for the auth result
-      client.waitAuthCmd = new Array();
-      session.doAuth(client, function(success) {
-        if (success != true) {
-          console.log('Invalid token provided by %s. Disconnecting.', client.remoteAddress);
-          client.end({ error: 'Invalid token' });
-          return;
-        }
-        // We have the auth result. Good.
-        for (dataMessage in client.waitAuthCmd)
-          command.handle(client, dataMessage);
-        delete command.waitAuthCmd;
-      });
+  if (data.params === undefined)
+    data.params = { };
+
+  // Find the command to execute
+  var foundCommand = false;
+  for (c in cmdList) {
+    if (cmdList[c].command != data.command)
+      continue;
+
+    if (cmdList[c].preAuth != true && session.isAuth(client) == false) {
+      console.warn('%s tried to execute a command without authenticating.', client.remoteAddress);
+      client.end(JSON.stringify({error: 'You must auth before running this command' }));
       return;
     }
 
+    foundCommand = true;
+    cmdList[c].handle(client, data.params);
+  }
 
-  } catch (ex) {
-    console.error('Could not handle packet from %. Closing connection.', client.remoteAddress);
-    client.end({ error: 'Could not handle packet' });
+  if (foundCommand == false) {
+    console.warn('%s tried to run a non-existant command', client.remoteAddress);
+    client.end(JSON.stringify({ error: 'No such command' }));
     return;
   }
 
